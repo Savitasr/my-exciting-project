7404867655592cb5cd613b77232661db5d2618a5
Savitasr <savitasvtmath@gmail.com>
2022-11-21 15:23:30 +0530

    commit

--- my-awesom-script.py
+++ my-awesom-script.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 print("Hajimemashite sekai!")
 print("Hello! World,how is it going?.")
 print("Hello! World,we are one!.")
+=======
+print("Hajimemashite sekai!")
+>>>>>>> 97a49c9 (last commit)
