52719c5d44e69c3b20cfc11e181b21c5f8cb51d9
Savitasr <savitasvtmath@gmail.com>
2022-11-21 16:05:49 +0530

    source change

--- my-awesom-script.py
+++ my-awesom-script.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 print("Hajimemashite sekai!")
 print("Hello! World,how is it going?.")
-print("Hello! World,we are one!.")
-=======
-print("Hajimemashite sekai!")
->>>>>>> 97a49c9 (last commit)
